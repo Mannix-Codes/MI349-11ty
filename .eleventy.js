4ef72c91406003246f4796bcfd5d101faca20a8e
Mannix Muir <155869270+Mannix-Codes@users.noreply.github.com>
2025-11-03 06:34:04 +0000

    Added more templating

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,31 @@
+const data = require('./information.json');
 
+
+function get_index(_title){
+  const idx = data.titles.indexOf(_title);
+  return idx;
+}
+
+function get_picture(img_index) {
+  const image=data.images[img_index];
+  const alt=data.alt[img_index];
+  return `<img src="/images/${image}" alt="${alt}" class="picture">`
+}
 // The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
 
-  eleventyConfig.addShortcode("picture", function(image, alt){
-    return `<img src="/images/${image}" alt="${alt}" class="picture">`
+  eleventyConfig.addShortcode("index_pic", function(title){
+    let _index = get_index(title);
+    return get_picture(_index)
   }
-)
-
+  )
+  eleventyConfig.addShortcode("index_text", function(title){
+    let _index = get_index(title);
+    let _text = data.text[_index];
+    return `<p>${_text}</p>`
+  }
+  )
 
 };
--- mouse.md
+++ mouse.md
@@ -5,7 +5,7 @@ title: mouse
 
 # Here is My Mouse
 <div class="contentpage">
-{% picture 'mouse.jpg' 'a computer mouse' %}
+{% index_pic 'keys' %}
 <p>This is my wireless mouse, the trackpad on my laptop has been getting finnicky recently and this has been a real life saver. Having to work on my computer every day makes it vital to my livelyhood
 </p>
 </div>
